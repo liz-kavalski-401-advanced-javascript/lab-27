619f76c63b24bd98c769f0db96bdafebd68c65e9
lizkavalski <lizkavalski@DESKTOP-IM8BUGM.localdomain>
2019-09-07 16:00:16 -0700

    updated gitinore

---
+++ build/precache-manifest.d1d16dd6320b2baa0950759a1aae01d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9f9308373ad567ea743ca4ebae4d82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcbdcd2b6307ed5c62b9",
+    "url": "/static/css/main.a23045db.chunk.css"
+  },
+  {
+    "revision": "af62fa7c12ab224ff571",
+    "url": "/static/js/2.02d8204d.chunk.js"
+  },
+  {
+    "revision": "dcbdcd2b6307ed5c62b9",
+    "url": "/static/js/main.242eb33f.chunk.js"
+  },
+  {
+    "revision": "49908bd30842a28eee79",
+    "url": "/static/js/runtime~main.92a49cf5.js"
+  }
+]);
